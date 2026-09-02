d2f8a96572cfd84f1b9dca72b1d9a99bd6ad4120
kalash <katyayankalash@gmail.com>
2026-09-02 20:11:45 +0530

    commit

---
+++ area_of_circle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int r;
+    float c;
+
+    printf("write radius of cicrle needed\n");
+    scanf("%d",&r);
+  c = 3.1416*r*r;
+
+  printf("your radius is %f", c);
+
+
+
+    return 0;
+}
